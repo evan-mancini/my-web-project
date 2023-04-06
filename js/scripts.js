a085b40380353b60cfc878add0a3e5a6a138a7bc
evan-mancini <elm8214@nyu.edu>
2023-04-06 17:03:47 -0400

    added informative table

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,4 @@
 $(document).ready(function () {
-    $("#mayors-list a").click(function (e) {
-      e.preventDefault();
-      var imageUrl = $(this).data("image");
-      $("#image-container").html('<img src="' + imageUrl + '">').show();
-    });
-  });
-  
+  $("#mayors-list a").click(function (e) {
+    e.preventDefault();
+    var imageUrl = $(
