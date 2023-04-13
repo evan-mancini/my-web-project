c90092ea479b71a5591b7a3808c5a1f85a012116
evan-mancini <elm8214@nyu.edu>
2023-04-12 23:34:31 -0400

    properly imported javascript and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,5 @@
-$(document).ready(function() {
-  $('#mayors-list a').click(function(e) {
+$(document).ready(function () {
+  $('#mayors-list a').click(function (e) {
     e.preventDefault();
     var imageUrl = $(this).data('image');
     $('#image-container').html('<img src="' + imageUrl + '">').show();
